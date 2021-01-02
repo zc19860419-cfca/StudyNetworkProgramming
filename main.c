315072bf939c88cb87d6d04810b4bf509bb4847f
zhangchong <509811327@qq.com>
2021-01-02 22:40:53 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,74 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netdb.h>
+
+
+/* 从socketfd描述字中读取"size"个字节. */
+size_t readn(int fd, void *buffer, size_t size) {
+    char *buffer_pointer = buffer;
+    int length = size;
+
+    while (length > 0) {
+        int result = read(fd, buffer_pointer, length);
+
+        if (result < 0) {
+            if (errno == EINTR)
+                continue;     /* 考虑非阻塞的情况，这里需要再次调用read */
+            else
+                return (-1);
+        } else if (result == 0)
+            break;                /* EOF(End of File)表示套接字关闭 */
+
+        length -= result;
+        buffer_pointer += result;
+    }
+    return (size - length);        /* 返回的是实际读取的字节数*/
+}
+
+void read_data(int sockfd) {
+    ssize_t n;
+    char buf[1024];
+
+    int time = 0;
+    for (;;) {
+        fprintf(stdout, "block in read\n");
+        if ((n = readn(sockfd, buf, 1024)) == 0)
+            return;
+
+        time++;
+        fprintf(stdout, "1K read for %d \n", time);
+        usleep(1000);//
+    }
+}
+
+
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(12345);
+
+    /* bind到本地地址，端口为12345 */
+    bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    /* listen的backlog为1024 */
+    listen(listenfd, 1024);
+
+    /* 循环处理用户请求 */
+    for (;;) {
+        clilen = sizeof(cliaddr);
+        connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
+        read_data(connfd);   /* 读取数据 */
+        close(connfd);          /* 关闭连接套接字，注意不是监听套接字*/
+    }
+}
